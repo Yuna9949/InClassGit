40630dc334631d9d47f698593249cd2a64d397d4
Yuna Oh <ustina0409@gmail.com>
2021-04-15 20:03:39 -0700

    generate random number

--- password.py
+++ password.py
@@ -3,5 +3,7 @@ import random
 def password_generator(length):
     password = ""
     alpabet = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*"
+    for i in range(length):
+        randomNum = random.randrange(len(alpabet))
 
 
